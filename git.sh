7de60958c2c56dfe28be01aaa73a711f42c26245
Lucki Alan Fernandez Salcedo <alan.lucki@gmail.com>
2022-10-17 12:11:49 -0500

    22/10/17-12:11:48

--- git.sh
+++ git.sh
@@ -1,5 +1,5 @@
 git config --global user.email "alan.lucki@gmail.com"
-git config --global user.name "alan.lucki@gmail.com"
+git config --global user.name "Lucki Alan Fernandez Salcedo"
 
 d=`date +%y/%m/%d-%H:%M:%S`
 
